741b9a685cc23bb2946cfff67fdf67e75b83cd28
松永俊輔 <matsunagashunsuke@matsunagashunsukenoMacBook-ea.local>
2018-11-06 20:50:42 +0900

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -5,13 +5,8 @@ from time import sleep
 import csv
 
 URL = "http://anzeninfo.mhlw.go.jp/anzen_pg/SAI_FND.aspx"
-<<<<<<< HEAD
-URL1 = "http://anzeninfo.mhlw.go.jp"
-print("aaaaaaaaaaaa")
-=======
 base_URL = "http://anzeninfo.mhlw.go.jp"
 
->>>>>>> c2890ffd2caf0803bf4ad9c3299daa7963e9bed9
 class Scrap:
     def get_html(self, url):
         r = requests.get(url)
@@ -53,48 +48,3 @@ for cases_path in sc.select_href(sc.get_html(URL),1):
 			writer = csv.writer(file)
 			writer.writerow(sc.select_href(sc.get_html(base_URL+case_path),3))
 			print("入れた")
-
-
-<<<<<<< HEAD
-for i in data:
-	soup2 = sc.sc1(URL1+i)
-	data2 = sc.sc2(soup2,2)
-	# print(data2)
-	for j in data2:
-		soup3 = sc.sc1(URL1+j)
-		soup3 = soup3.find("table", class_="tbl_1_1_1")
-		print(soup3)
-		sleep(1)
-=======
-# soup = sc.sc1(URL)
-# data = sc.sc2(soup,1)
-# print(data)
-# with open('result.csv', 'a') as file:
-# 	result = []
-# 	for i in data:
-# 		soup2 = sc.sc1(URL1+i)
-# 		for data2 in sc.sc2(soup2,2):
-# 			print(data2)
-# 			soup3 = sc.sc1(URL1+data2)
-# 			soup3 = soup3.find("table", class_="tbl_1_1_1")
-# 			soup3 = soup3.find_all("td")
-# 			l = []
-# 			for j in soup3:
-# 				if j != None:
-# 				    print(j.text.split("\t")[18])
-# 				    l.append(j.text.split("\t")[18])
-		
-# 			# print(soup3.text)
-# 			# hoge = str(soup3.text)
-# 			# hoge = hoge.replace("\t","")
-# 			# hoge = hoge.split("\n")
-# 			# print(hoge)
-# 			# hoge = hoge.remove("a")
-# 			#print(hoge)
-# 			# hoge2 = hoge.split(" ")
-# 			# print(hoge2)
-# 			print(l)
-# 			writer = csv.writer(file, lineterminator = '\n')
-# 			writer.writerow(l)
-# 			print("入れた")
->>>>>>> c2890ffd2caf0803bf4ad9c3299daa7963e9bed9
